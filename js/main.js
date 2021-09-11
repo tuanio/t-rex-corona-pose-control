8d0d0a596705c7116da0170a06be6202f130a5f6
MIT Root <phamthinh@pop-os.localdomain>
2021-09-11 18:52:11 +0700

    Thing commit

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ btnPlay.addEventListener("click", () => {
   btnPlay.innerText = "Nguyen Van Anh Tuan is playing ...";
   btnPlay.disabled = true;
 });
+
+async function ring(state) {
+  let run = await document.querySelector('img#state-run');
+  let jump = await document.querySelector('img#state-jump');
+
+  if (state == 1) {
+    jump.style.display = 'block';
+    run.style.display = 'none';
+  }
+  else {
+    jump.style.display = 'none';
+    run.style.display = 'block';
+  }
+}
+
+ring(0);
