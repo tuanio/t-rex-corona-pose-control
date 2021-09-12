a71380d03065798aa6d06ceedfef7795a6b89dfb
MIT Root <phamthinh@pop-os.localdomain>
2021-09-12 17:32:00 +0700

    Ok con de

--- js/main.js
+++ js/main.js
@@ -21,18 +21,45 @@ btnPlay.addEventListener("click", () => {
   btnPlay.disabled = true;
 });
 
+// Hiệu ứng đám mây
 async function ring(state) {
   let run = await document.querySelector('img#state-run');
   let jump = await document.querySelector('img#state-jump');
+  let dead = await document.querySelector('img#state-dead');
 
-  if (state == 1) {
+  // Chưa chơi
+  if (state == 0) {
+    jump.style.display = 'none';
+    run.style.display = 'none';
+    dead.style.display = 'none';
+  }
+  // Đang nhảy
+  else if (state == 1) {
     jump.style.display = 'block';
     run.style.display = 'none';
   }
-  else {
+  // Đang chạy
+  else if (state == 2) {
     jump.style.display = 'none';
     run.style.display = 'block';
   }
+  // Đang chết
+  else {
+    jump.style.display = 'none';
+    run.style.display = 'none';
+    dead.style.display = 'block';
+  }
+
+}
+ring(2);
+
+function account_info() {
+  alert('Hello !');
 }
 
-ring(0);
+// async function account() {
+//   let loc_account = await document.getElementByIdt('account');
+//   if (loc_account.onclick == true) {
+//     account_info();
+//   }
+// }
