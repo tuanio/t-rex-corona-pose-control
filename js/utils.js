e0a9f4c37d9e7807eada41498d1d0873a8c44b41
Nguyễn Văn Anh Tuấn <nvatuan3@gmail.com>
2021-09-14 18:38:42 +0700

    OK

--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,5 @@
-var frontendUrl = 'http://127.0.0.1:5501';
+// var frontendUrl = 'http://127.0.0.1:5501';
+var frontendUrl = 'https://tuanio.github.io/t-rex-corona-pose-control'
 // var backendUrl = 'http://127.0.0.1:5000';
 var backendUrl = 'https://backend-runner.herokuapp.com';
 var loginUrl = frontendUrl + '/login.html';
