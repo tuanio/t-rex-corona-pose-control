763818f06de60926ecf5dc6a3c0c790c9d472e7c
Nguyễn Văn Anh Tuấn <nvatuan3@gmail.com>
2021-09-15 22:47:28 +0700

    OK

--- js/utils.js
+++ js/utils.js
@@ -54,25 +54,25 @@ async function checkAuthorization() {
 if (window.location.href == frontendUrl + '/index.html' || window.location.href == frontendUrl + '/' || window.location.href == frontendUrl + '/login.html') {
     setInterval(checkAuthorization, 2000);
 }
-// document.onkeydown = function (evt) {
-//     //  All shortcut devtools
-//     // F12
-//     if (evt.keyCode === 123) return false;
+document.onkeydown = function (evt) {
+    //  All shortcut devtools
+    // F12
+    if (evt.keyCode === 123) return false;
 
-//     if (evt.ctrlKey && evt.keyCode == 'U'.charCodeAt(0)) return false;
-//     if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'I'.charCodeAt(0)) return false;
-//     if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'J'.charCodeAt(0)) return false;
-//     if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'C'.charCodeAt(0)) return false;
-//     if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'E'.charCodeAt(0)) return false;
-//     if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'K'.charCodeAt(0)) return false;
-//     if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'M'.charCodeAt(0)) return false;
-//     if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'Z'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.keyCode == 'U'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'I'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'J'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'C'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'E'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'K'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'M'.charCodeAt(0)) return false;
+    if (evt.ctrlKey && evt.shiftKey && evt.keyCode == 'Z'.charCodeAt(0)) return false;
 
-//     // Shift + F4 -> F9
-//     if (evt.shiftKey && evt.keyCode === 115) return false;
-//     if (evt.shiftKey && evt.keyCode === 116) return false;
-//     if (evt.shiftKey && evt.keyCode === 117) return false;
-//     if (evt.shiftKey && evt.keyCode === 118) return false;
-//     if (evt.shiftKey && evt.keyCode === 119) return false;
-//     if (evt.shiftKey && evt.keyCode === 120) return false;
-// };
+    // Shift + F4 -> F9
+    if (evt.shiftKey && evt.keyCode === 115) return false;
+    if (evt.shiftKey && evt.keyCode === 116) return false;
+    if (evt.shiftKey && evt.keyCode === 117) return false;
+    if (evt.shiftKey && evt.keyCode === 118) return false;
+    if (evt.shiftKey && evt.keyCode === 119) return false;
+    if (evt.shiftKey && evt.keyCode === 120) return false;
+};
